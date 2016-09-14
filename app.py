12ebf7f38059c3f61503e3e2985880978446ad90
Abhay <indiunix@gmail.com>
2016-09-14 13:55:01 +0530

    Before Nltk

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for
 from config import Config, DevelopmentConfig
 from flask_sqlalchemy import SQLAlchemy
 import os
+
+import requests
+import re
+import nltk
+import operator
+from collections import Counter
+from lxml import html
+from stop_words import stops
+
+
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
@@ -10,9 +20,18 @@ db = SQLAlchemy(app)
 
 from models import Result
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    return 'Hello World!'
+    errors = []
+    results = {}
+    if request.method == "POST":
+        try:
+            url = request.form['url']
+            r = requests.get(url)
+            print(r.text)
+        except:
+            errors.append('Unable to get URL')
+    return render_template('index.html', errors=errors, results=results)
 
 @app.route('/<name>')
 def hello_name(name):
